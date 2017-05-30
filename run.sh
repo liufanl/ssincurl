b25401fece9257aa8970d297ecd9193408dbd551
liufanl <liufanl@users.noreply.github.com>
2017-05-31 00:17:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i 's/user/'$user'/g' /var/www/html/api.php
-sed -i 's/passwd/'$passwd'/g' /var/www/html/api.php
+sed -i 's/user/'"$user"'/g' /var/www/html/api.php
+sed -i 's/passwd/'"$passwd"'/g' /var/www/html/api.php
 //sed -i 's/jaz-ss/'"$ssname"'/g' /var/www/html/index.html
 
 apache2-foreground
