359e7899e9330ba7d58b5e79b339d6cfade20cb8
liufanl <liufanl@users.noreply.github.com>
2017-05-31 01:07:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+echo "user:"$user
+echo "passwd:"$passwd
 sed -i 's/user/'"$user"'/g' /var/www/html/api.php
 sed -i 's/passwd/'"$passwd"'/g' /var/www/html/api.php
 sed -i 's/jaz-ss/'"$ssname"'/g' /var/www/html/index.html
